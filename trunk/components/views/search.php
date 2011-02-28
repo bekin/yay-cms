f6e09dd3757b9563019c0da4e52b87f3a2de7bf1
thyseus <thyseus@788895c9-0f5c-1c4f-6d22-a9ea875d146d>
2011-02-28 13:46:36 +0000

    added submit button to search widget

--- trunk/components/views/search.php
+++ trunk/components/views/search.php
@@ -1,5 +1,6 @@
 <?php
 echo CHtml::beginForm(array('//cms/sitecontent/search'));
 echo CHtml::textField('search', '', array('size' => 10));
+echo CHtml::submitButton('Suche');
 echo CHtml::endForm();
 ?>
